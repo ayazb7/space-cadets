09b862fdafd3fdc2f7f18c8185ab090d21dcbe2f
ayazb7 <60544937+ayazb7@users.noreply.github.com>
2021-10-18 22:25:45 +0100

    Add files via upload

--- NameFromEmail.java
+++ NameFromEmail.java
@@ -10,13 +10,13 @@ public class NameFromEmail {
     System.out.println("Enter email: ");
     Scanner input = new Scanner(System.in);
 
-    String emailInput = input.next();
+    String emailInput = input.next(); //User inputs an email address
 
-    String nameCode = emailInput.substring(0, emailInput.indexOf('@'));
+    String nameCode = emailInput.substring(0, emailInput.indexOf('@')); //Substrings the email address to just the name code
 
-    Document doc = Jsoup.connect("https://www.ecs.soton.ac.uk/people/"+nameCode).get();
-    String title = doc.getElementsByTag("title").get(0).text();
-    String name = title.substring(0, title.indexOf('|'));
+    Document doc = Jsoup.connect("https://www.ecs.soton.ac.uk/people/"+nameCode).get(); //Jsoup used to get the html file of the ecs soton link
+    String title = doc.getElementsByTag("title").get(0).text(); //Obtains the string held in the title element
+    String name = title.substring(0, title.indexOf('|')); //Substrings the title to get the full name only
     System.out.println(name);
   }
 }
